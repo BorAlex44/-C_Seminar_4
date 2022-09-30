083fb00e70b71aba1b1451b422f7cb2ae0d3948d
BorAlex <alexander44@bl.ru>
2022-09-30 21:11:21 +0300

    add Example4_29

---
+++ Example4_29/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+Clear();
+int size = 8;
+int[] array = GetRandomArray(size);
+PrintArray(array);
+
+
+
+
+
+
+
+
+
+
+
+int[] GetRandomArray(int lenght)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        result[i] = new Random().Next(0, 101);
+    }
+    return result;
+}
+
+void PrintArray(int[] MyArray)
+{
+    Write("[");
+    for (int i = 0; i < MyArray.Length - 1; i++)
+    {
+        Write($"{MyArray[i]},");
+    }
+    Write($"{MyArray[MyArray.Length - 1]}]");
+}
