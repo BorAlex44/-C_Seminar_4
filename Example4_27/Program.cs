533d5d142d75ec7dacb68e358b852e71bb93626a
BorAlex <alexander44@bl.ru>
2022-09-30 15:54:55 +0300

    add Example4_27

---
+++ Example4_27/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int num = int.Parse(ReadLine()!);
+int summa = GetSumma(num);
+WriteLine($"Сумма цифр числа {num} равна {summa}");
+
+
+
+
+
+
+
+
+
+int GetSumma(int number)
+{
+    int result = 0;
+    int temp = 0;
+    while (number > 0)
+    {
+        temp = number % 10;
+        result = result + temp;
+        number = number / 10;
+    }
+    return result;
+}
